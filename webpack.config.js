79c6afc21a1af8e404472eeee92b05d38d353581
Shanshan Xu <tinaxu84@gmail.com>
2021-04-03 16:38:34 -0400

    split the files

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const path = require("path");
 const webpack = require('webpack');
 
@@ -5,10 +6,38 @@ const webpack = require('webpack');
 
 module.exports = {
     // webpack configuration with three properties: entry, output, and mode
-    entry: './assets/js/script.js',
+    entry: {
+        app: './assets/js/script.js',
+        events: './assets/js/events.js',
+        schedule: './assets/js/schedule.js',
+        tickets: './assets/js/tickets.js'
+    },
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.bundle.js'
+        filename: '[name].bundle.js',
+        path: __dirname + 'dist'   
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jpg$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name (file) {
+                                return "[path][name].[ext]"
+                            },
+                            publicPath: function(url) {
+                                return url.replace("../", "/assets/")
+                            }
+                        }
+                    },
+                    {
+                        loader: 'image-webpack-loader'
+                    }
+                ]
+            }
+        ]
     },
     // tell webpack to use jquery npm package
     plugins: [
@@ -16,6 +45,10 @@ module.exports = {
             $: 'jquery',
             jQuery: 'jquery'
         }),
+        new BundleAnalyzerPlugin({
+            // the report outputs to an HTML file in the dist folder
+            analyzerMode: 'static',
+        })
     ],
     mode: 'development'
 };
